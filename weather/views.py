e0eb32c7b16147ded68dba3ee6686f3ace93a610
michael basweti <baswetima@gmail.com>
2022-09-13 20:16:11 +0300

    Add index route

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import requests
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class index(APIView):
+
+    def get(self, request):
+        message = {
+            "message":"Hello, this API returns data"
+        }
+
+        return Response(message, status=status.HTTP_200_OK)
